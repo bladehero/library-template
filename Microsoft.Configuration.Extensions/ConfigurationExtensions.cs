04c14a7e7bb24998a45cbd82a89026903d755d93
Nikita Dermenzhi <nikita.dermenzhi@gmail.com>
2024-10-27 20:30:56 +0200

    feat: add configuration extensions

---
+++ Microsoft.Configuration.Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Configuration.Extensions;
+
+public static class ConfigurationExtensions
+{
+    public static IServiceCollection AddConfiguration<T>(
+        this IServiceCollection services,
+        IConfiguration configuration,
+        string? name = null
+    )
+        where T : class => services.Configure<T>(configuration.GetSection(name ?? typeof(T).Name));
+
+    public static T? GetSectionAs<T>(this IConfiguration configuration, string? sectionName = null) =>
+        configuration.GetSection(sectionName ?? typeof(T).Name).Get<T>();
+
+    public static object? GetSectionByType(
+        this IConfiguration configuration,
+        Type type,
+        string sectionName
+    ) => configuration.GetSection(sectionName).Get(type);
+}
